ef060b28f4b5ada49a5bcb8116ba0af9487a2db2
ANO-DeVv <88611334+ANO-DeVv@users.noreply.github.com>
2021-09-14 11:33:58 +0300

    Create Ano.js

---
+++ Ano.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === 'wara') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("841792552314994709").send(
+`> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`PARTNER ANO`) 
+});
+
+
+
+
+client.login("");// Token Dane 
